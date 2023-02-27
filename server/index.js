b511e11c0b8a832e91108f6c5129026117421bfb
awaisfiaz <awaisfiaz360@gmail.com>
2023-02-27 21:34:21 +0500

    connecting server to the database

--- server/index.js
+++ server/index.js
@@ -8,3 +8,17 @@ const app = express();
 app.use(bodyParser.json({ limit: "30mb", extennded: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extennded: true }));
 app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://javascriptmastery:javascriptmastery123@cluster0.y6esplz.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
